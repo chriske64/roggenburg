15e1215bab70d74ef6c545c9231b0f5b5b225ed0
Christoph Kehle <christoph.kehle@gmail.com>
2017-08-28 20:44:37 +0200

    added nice if else clause

--- loss.m
+++ loss.m
@@ -1,7 +1,7 @@
 function energy = loss(W,samples,alpha)
 %extract sample size
 amountData = size(samples,2);
-
+if alpha ~= 0
     for i=1:amountData
         %extract one sample from samples
         onesample = samples(:,i);
@@ -9,4 +9,14 @@ amountData = size(samples,2);
         label = onesample(length(onesample));
         energy =  energy + log(norm(exp(W' * image),1) - dot(image,W(:,label))) + alpha .* norm(W,'fro');
     end
+else
+    for i=1:amountData
+        %extract one sample from samples
+        onesample = samples(:,i);
+        image = onesample(1:length(onesample)-1);
+        label = onesample(length(onesample));
+        energy =  energy + log(norm(exp(W' * image),1) - dot(image,W(:,label)));
+    end
+end
+
 end
