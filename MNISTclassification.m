848486d6e554c78e0bd8c27ae2f649440d84e0b2
SebLu1 <sl767@cam.ac.uk>
2017-08-29 10:19:17 +0100

    fixed chrises bugs

--- MNISTclassification.m
+++ MNISTclassification.m
@@ -1,18 +1,19 @@
 %% load images and set up problem - nothing to be done here
 images = loadMNISTImages('train-images.idx3-ubyte');
 labels = loadMNISTLabels('train-labels.idx1-ubyte');
-figure, imagesc(reshape(images(:,1), [28,28])); colormap gray; axis off; 
+images_test = loadMNISTImages('t10k-images.idx3-ubyte');
+labels_test = loadMNISTLabels('t10k-labels.idx1-ubyte');
+
 
 % initial weights W
 W = 0.1*randn(28*28,10);
 
 %% Implement the minimization algorithm here!
+samples = get_samples(images, labels, 50);
+
 
 
 %% Test your result - nothing to do here - the code works as is
-images_test = loadMNISTImages('t10k-images.idx3-ubyte');
-labels_test = loadMNISTLabels('t10k-labels.idx1-ubyte');
-
 num_test = size(images_test, 2);
 [~,predictedLabels] = max(images_test'*W,[],2);
 correct = sum(((predictedLabels - labels_test -1)==0));
--- get_samples.m
+++ get_samples.m
@@ -1,4 +1,4 @@
-function [ samples ] = get_samples(images,labels, amount)
+function [ samples ] = get_samples(images, labels, amount)
 %get_samples Takes as an input amout of samples to produce and the images and labels. The input of images is 28^2 x [amout of train/test images]. Outputs samples
 %in format R^(28^2 +1) x R^amout
 
@@ -11,7 +11,7 @@ else
 
 which_samples = randperm(size(images,2),amount);
 images = images(:,which_samples);
-labels = labels(:,which_samples);
+labels = labels(which_samples,:);
 samples(1:28^2,:) = images;
 samples(28.^2 +1,:) = labels;
     
