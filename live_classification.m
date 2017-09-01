322d10707b27670d59628959229292521bbcd165
Christoph Kehle <christoph.kehle@gmail.com>
2017-09-01 17:35:02 +0200

    small changes

--- live_classification.m
+++ live_classification.m
@@ -1,27 +1,47 @@
-clc;
 clear;
-label='nothing';
-cam = webcam;
+
+%% Select camera
+camera = 'internal'; %set either to 'phone' or 'internal'
+
+
+if strcmp(camera,'phone')
+cam = webcam(1);
+% note: resolution = '480x640';
+elseif strcmp(camera,'internal')
+cam = webcam(2);
+% note: resolution = '1280x720';
+end
+
+%% Load trained weights W
 learned_W = matfile('saveW.mat');
 W = learned_W.W;
 
 figure;
-
+%%
 while true
 
 pic = cam.snapshot();
 
-picg = rgb2gray(pic);
-picg = imcrop(picg,[390,60,500,500]);
-picg = imresize(picg, [28,28]);
-picg = imcomplement(picg);
-picg = imadjust(picg);
+pic_segm = rgb2gray(pic);
 
-picg = thresholding(double(picg),160);
 
-picg =  double(reshape((picg),[28.^2,1]))/255;
+if strcmp(camera,'internal')
+pic_segm = imcrop(pic_segm,[390,60,500,500]);
+pic_segm = imresize(pic_segm, [28,28]);
 
-probs = picg'*W;
+
+elseif strcmp(camera,'phone')
+pic_segm = imcrop(pic_segm,[120,50,400,400]);
+end
+
+
+pic_segm = imcomplement(pic_segm);
+pic_segm = imadjust(pic_segm);
+pic_segm = thresholding(double(pic_segm),130);
+
+pic_segm = normalize(pic_segm, 0.3)./255;
+
+probs = pic_segm'*W;
 [maxprob,label] = max(probs);
 probs = exp(probs)/sum(exp(probs));
 
@@ -29,10 +49,13 @@ probs = exp(probs)/sum(exp(probs));
 subplot(1,3,1);
 imshow(pic);
 hold on;
+if strcmp(camera, 'internal')
 rectangle('Position', [390,60,500,500], 'LineWidt',4, 'Edgecolor','r' );
-hold off;
+elseif strcmp(camera, 'phone')
+rectangle('Position', [120,50,400,400], 'LineWidt',4, 'Edgecolor','r' );
+end
 subplot(1,3,2);
-imshow(reshape(picg, [28,28]));
+imshow(reshape(pic_segm, [28,28]));
 title(['This is a ', num2str(label-1), ' with probability ', num2str(probs(label)*100), '%']);
 subplot(1,3,3);
 bar(0:1:9,100*probs);
