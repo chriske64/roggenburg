14d4b79cad933fa8a0843bff129810763d91f552
Christoph Kehle <christoph.kehle@gmail.com>
2017-09-04 11:42:54 +0200

    modified the get samples file

--- get_samples_preprocessed.m
+++ get_samples_preprocessed.m
@@ -1,4 +1,4 @@
-function [ samples ] = get_samples(images, labels, amount)
+function [ samples ] = get_samples_preprocessed(images, labels, amount, learn_preprocessing)
 %get_samples Takes as an input amout of samples to produce and the images and labels. The input of images is 28^2 x [amout of train/test images]. Outputs samples
 %in format R^(28^2 +1) x R^amout
 
@@ -11,7 +11,11 @@ else
 
 which_samples = randperm(size(images,2),amount);
 images = images(:,which_samples);
-
+if learn_preprocessing == true
+    images = reshape(images,[28,28,amount]);
+    images = uint8(255*images);
+    images = imreshape();
+end
 labels = labels(which_samples,:);
 samples(1:28^2,:) = images;
 samples(28.^2 +1,:) = labels;
